0d4876fa3d812413e50888e36c580a0ec86ebf67
silence_zhpf <silence_zhpf@aliyun.com>
2023-09-13 10:52:48 +0800

    ✨ feat: 添加主题切换

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
 import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
 import rehypeMathjax from "rehype-mathjax";
 
 // https://astro.build/config
